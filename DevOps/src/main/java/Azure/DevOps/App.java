5b4a1de1c7604556df37e93b2591b41b8d7522f8
ManojTambe <ManojTambe@Manoj_Tambe_FCT>
2024-05-26 22:59:23 +0530

    e

--- DevOps/src/main/java/Azure/DevOps/App.java
+++ DevOps/src/main/java/Azure/DevOps/App.java
@@ -1,7 +1,7 @@
 package Azure.DevOps;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.remote.RemoteWebDriver;
 
 /**
  * Hello world!
@@ -9,12 +9,12 @@ import org.openqa.selenium.remote.RemoteWebDriver;
  */
 public class App 
 {
-	RemoteWebDriver driver = null;
+//	RemoteWebDriver driver = null;
     public void Azure()
     {
         System.out.println( "Hello World!" );
-        driver = new ChromeDriver();
-        driver.get("https://www.meta.com/");
-        driver.quit();
+//        driver = new ChromeDriver();
+//        driver.get("https://www.meta.com/");
+//        driver.quit();
     }
 }
